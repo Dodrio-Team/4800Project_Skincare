629263fc68d41fb4009ec6e63ddc692510a96a40
Katie Pham <klpham137@gmail.com>
2023-10-04 18:33:19 -0700

    running siimple test case

---
+++ reactapp/src/testing/screenTest.test.js
@@ -0,0 +1,11 @@
+test('it should return the correct array length', () => {
+    // Arrange: Create an array
+    const myArray = [1, 2, 3, 4, 5];
+  
+    // Act: Get the length of the array
+    const arrayLength = myArray.length;
+  
+    // Assert: Check if the length matches the expected value
+    expect(arrayLength).toBe(5); // Expecting the array length to be 5
+  });
+  
