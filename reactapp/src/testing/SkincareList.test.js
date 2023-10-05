b40721fa3a0d3de8d36479a8f39a73cebf9bd00f
tiff178 <tiff178@gmail.com>
2023-10-04 18:20:26 -0700

    test case - Tiff

---
+++ reactapp/src/testing/SkincareList.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+test('tiff-test', () => {
+  expect(true).toBe(true)
+}
+)
---
+++ reactapp/src/testing/test.test.js
@@ -0,0 +1,4 @@
+test('tiff-test', () => {
+    expect(true).toBe(true)
+}
+)
