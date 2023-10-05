7144fb763c411f9c7bf6ba8e26d01e6bd45140c6
BCStr <bvchhor@cpp.edu>
2023-10-04 18:27:37 -0700

    test case | Brian

---
+++ reactapp/src/testing/SC.test.js
@@ -0,0 +1,12 @@
+test('sample-test', () => {
+    const value = 42; // Modify this value as per your test case
+    
+    expect(value).toEqual(42); // Replace 42 with the expected value for your test case
+});
+describe('sample-test-suite', () => {
+    it('should pass', () => {
+        const value = 42;
+        expect(value).toEqual(42);
+    });
+
+});
