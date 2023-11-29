208ab34be45124254709bf4d671b330ff9c7e9dd
tiff178 <tiff178@gmail.com>
2023-11-28 16:01:10 -0800

    fixed nav bar links and added feedback form

--- reactapp/src/FrontPage.js
+++ reactapp/src/FrontPage.js
@@ -11,15 +11,12 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import ProductPage from './ProductsPage';
 
-const pages = ['Products', 'Pricing'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Products', 'Feedback', 'Blog'];
 
 function FrontPage() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -31,6 +28,7 @@ function FrontPage() {
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
+  
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -38,10 +36,12 @@ function FrontPage() {
   const handleCloseNavMenu = (page) => {
     setAnchorElNav(null);
     if (page === 'blog') {
-      window.location.href = 'https://forms.gle/C7r7nG2r81o6C2gz7'; // Replace with the actual blog link
-    } else if (page === 'product') {
+      window.location.href = 'https://cs480-projects.github.io/teams-fall2023/DodrioTeam/index.html'; // Replace with the actual blog link
+    } else if (page === 'products') {
       navigate('/products'); // Redirect to the ProductsPage
-    } 
+    } else if (page === 'feedback') {
+      openGoogleForm();
+    }
   };
 
   const handleCloseUserMenu = () => {
@@ -121,36 +121,17 @@ function FrontPage() {
                 }}
               >
                 {pages.map((page) => (
-                  <MenuItem key={page} onClick={() => handleCloseNavMenu(page)}>
+                  <MenuItem key={page} onClick={() => handleCloseNavMenu(page.toLowerCase())}>
                     <Typography textAlign="center">{page}</Typography>
                   </MenuItem>
                 ))}
               </Menu>
             </Box>
-            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'black',
-                textDecoration: 'none',
-              }}
-            >
-              Flawless
-            </Typography>
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
               {pages.map((page) => (
                 <Button
                   key={page}
-                  onClick={handleCloseNavMenu}
+                  onClick={() => handleCloseNavMenu(page.toLowerCase())}
                   sx={{ my: 2, color: 'black', display: 'block', fontFamily: 'monospace', fontSize: 16 }}
                 >
                   {page}
@@ -159,9 +140,6 @@ function FrontPage() {
             </Box>
 
             <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-              <button className="feedback-button" onClick={openGoogleForm}>
-                FeedBack Form
-              </button>
               <input
                 type="text"
                 value={searchQuery}
@@ -175,37 +153,6 @@ function FrontPage() {
                 Search
               </button>
             </Box>
-
-                
-            {/* <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box> */}
           </Toolbar>
         </Container>
       </AppBar>
@@ -221,18 +168,12 @@ function FrontPage() {
     }}>
       <h2>Welcome to Flawless! Explore the different budget-friendly products for your skintype</h2>
       <div className="button-container">
-        {/* <Link>
-          <button className="custom-button">Visit our Products</button>
-        </Link> */}
       <Link to="/products"> {ProductPage}
         <button className="custom-button">Visit our Products</button>
       </Link>
 
       </div>
     </Box>
-    {/* <button className="search-button" onClick={openGoogleForm}>
-        FeedBack Form
-    </button> */}
     </div>  
   );
 }
